57bfe0ce876d7906d97cc55fa67b96bde97a65bd
k2madureira <lenilsonmadureira@gmail.com>
2020-02-04 18:30:50 -0300

    feature: deploy

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,7 +7,7 @@ class UserController {
     const checkEmail = await User.findOne({ where: {email} });
 
     if (checkEmail) {
-      return res.status(400).json({ error: 'Duplicated email !!'});
+      return res.status(400).json({ error: 'Duplicated email!!'});
     }
 
     const user = await User.create(req.body);
